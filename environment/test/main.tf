9605c12928e65e03ae14c9653b8bd952f5597c2f
Pswamy <pswamypinapothu@gmail.com>
2025-08-17 00:49:03 -0700

    test

--- environment/test/main.tf
+++ environment/test/main.tf
@@ -1,25 +1,25 @@
    module "s3_bucket_module" {
-    source = "../Modules/S3"
+    source = "../../Modules/S3"
     bucket_name = var.bucket_name
     env = var.env
     bucket_status = var.Test_s3_status1
    }
 
    module "Test_s3" {
-    source = "../Modules/S3"
+    source = "../../Modules/S3"
     bucket_name = var.bucket_name1
     env = var.env
     bucket_status = var.Test_s3_status
    }
 
     module "s3_Nick" {
-    source = "../Modules/S3"
+    source = "../../Modules/S3"
     bucket_name = var.bucket_name2
     env = var.env
     #bucket_status = var.Test_s3_status1
    }
     module "s3_Nick2" {
-    source = "../Modules/S3"
+    source = "../../Modules/S3"
     bucket_name = var.bucket_name4
     env = var.env
     bucket_status = var.Test_s3_status4
@@ -31,7 +31,7 @@
    #Security group
    #=========================================================================
    module "sg_name_module" {
-      source = "../Modules/SG"  
+      source = "../../Modules/SG"  
       sg_name = var.sg_name
       vpc_id = var.vpc_id
 
